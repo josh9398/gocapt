63d57212afe6f965498a99dc919e9d008c9438fc
Joshua Bowen <josh9398@hotmail.com>
2019-01-18 21:01:14 +0000

    Packet capturing example

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
     "fmt"
     "log"
     "./core"
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/pcap"
 )
 
 func main() {
@@ -13,4 +15,14 @@ func main() {
         log.Fatal(err)
     }
     fmt.Println(*opts.InterfaceName)
+	if handle, err := pcap.OpenLive(*opts.InterfaceName, 1500, true, pcap.BlockForever); err != nil {
+		panic(err)
+	} else if err := handle.SetBPFFilter("tcp and port 80"); err != nil {  // optional
+		panic(err)
+	} else {
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			fmt.Println(packet)  // Do something with a packet here.
+		}
+	}
 }
